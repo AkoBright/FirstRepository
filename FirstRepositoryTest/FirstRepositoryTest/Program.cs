a8f5a53991d397eeddd654a8316c37b6c901bca2
Akoran <gitbright@gmail.com>
2017-04-06 10:33:52 +0200

    Added code.

---
+++ FirstRepositoryTest/FirstRepositoryTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstRepositoryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = "";
+            while (n!="2")
+            {
+                Console.WriteLine("Type 2 to exit");
+                n=Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
